816a4716613d7e2a5e688c53bbdaa56425e5fc4b
bruce-mig <brucetmig@icloud.com>
2024-03-26 21:51:37 +0200

    added data constraints and validation annotations

--- src/main/java/com/github/bruce/mig/runnerz/run/Run.java
+++ src/main/java/com/github/bruce/mig/runnerz/run/Run.java
@@ -1,13 +1,28 @@
 package com.github.bruce.mig.runnerz.run;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
+
 import java.time.LocalDateTime;
 
 public record Run(
+        @Id
         Integer id,
+        @NotEmpty
         String title,
         LocalDateTime startedOn,
         LocalDateTime completedOn,
+        @Positive
         Integer miles,
-        Location location
+        Location location,
+        @Version
+        Integer version
 ) {
+    public Run {
+        if (!completedOn.isAfter(startedOn)) {
+            throw new IllegalArgumentException("Completed On must be after Started On");
+        }
+    }
 }
